ce2185207d2657b28751885f03a077081847bd4a
kimyurim <yfla980107@gmail.com>
2021-08-03 15:53:55 +0900

    함수1

---
+++ Function1.py
@@ -0,0 +1,17 @@
+# Function1.py
+# 리턴을 하지 않는 함수
+def setValue(newValue):
+    #지역변수
+    c = newValue
+    print("지역변수:", x)
+
+#호출
+result = setValue(5)
+print(result)
+
+def swap(x,y):
+    return y,x
+
+# 호출
+result = swap(3,4)
+print(result)
